69b901c079202877e18a0f1284e6f47f2a5a1309
IdaszakDaniel <idaszak1@gmail.com>
2019-01-22 23:07:24 +0100

    fetching movies from facebook account

--- src/components/loginComponent.js
+++ src/components/loginComponent.js
@@ -1,41 +1,88 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Button } from 'react-native';
 import { LoginButton, AccessToken } from 'react-native-fbsdk';
-import { GraphRequest, GraphRequestManager } from 'react-native-fbsdk';
+import { GraphRequest, GraphRequestManager, LoginManager } from 'react-native-fbsdk';
 
 export default class Login extends Component {
 
-  _responseInfoCallback(error, result) {
-    if (error) {
-      console.log('Error fetching data: ' + error.toString());
-    } else {
-      console.log('Success fetching data: ', {result});
-    }
-  }
+  // _responseInfoCallback(error, result) {
+  //   if (error) {
+  //     console.log('Error fetching data: ' + error.toString());
+  //   } else {
+  //     console.log('Success fetching data: ', {result});
+  //   }
+  // }
 
-  startRequest(token) {
-    console.log('starteddddd')
-    const infoRequest = new GraphRequest(
-      '/me',
-      {
-        parameters: {
-          fields: {
-            string: 'email, name, first_name, middle_name, last_name, picture.type(large)'
-          },
-          access_token: {
-            string: token
-          }
+  // startRequest(token) {
+  //   console.log('starteddddd')
+  //   const infoRequest = new GraphRequest(
+  //     '/me',
+  //     {
+  //       parameters: {
+  //         fields: {
+  //           string: 'id, email, name, first_name, middle_name, last_name, picture.type(large)'
+  //         },
+  //         access_token: {
+  //           string: token
+  //         }
+  //       }
+  //     },
+  //     this._responseInfoCallback,
+  //   );
+
+  //   const movieRequest = new GraphRequest(
+  //     '/10216755940084848/music',
+  //     {
+  //       accessToken: token
+  //     },
+  //     this._responseInfoCallback,
+  //   );
+
+  //   new GraphRequestManager().addRequest(movieRequest).start();
+  // }
+
+  login(){
+    LoginManager.logInWithReadPermissions(["public_profile", "user_likes"]).then(
+      function(result) {
+        if (result.isCancelled) {
+          console.log("Login cancelled");
+        } else {
+          console.log(
+            "Login success with permissions: " +
+              result.grantedPermissions.toString()
+          );
+          AccessToken.getCurrentAccessToken().then(
+            (data) => {
+              let token = data.accessToken.toString();
+              const _responseInfoCallback = (error, result) => {
+                if (error) {
+                  console.log('Error fetching data: ' + error.toString());
+                } else {
+                  console.log('Success fetching data: ', {result});
+                }
+              }
+              const movieRequest = new GraphRequest(
+                '/10216755940084848/movies',
+                {
+                  accessToken: token
+                },
+                _responseInfoCallback,
+              );
+              new GraphRequestManager().addRequest(movieRequest).start();
+            }
+          )
         }
       },
-      this._responseInfoCallback,
+      function(error) {
+        console.log("Login fail with error: " + error);
+      }
     );
-    new GraphRequestManager().addRequest(infoRequest).start();
   }
   
   render() {
     return (
       <View>
-        <LoginButton
+        {/* <LoginButton
           onLoginFinished={
             (error, result) => {
               if (error) {
@@ -53,7 +100,8 @@ export default class Login extends Component {
               }
             }
           }
-          onLogoutFinished={() => console.log("logout.")}/>
+          onLogoutFinished={() => console.log("logout.")}/> */}
+          <Button title="LOGIN" onPress={this.login}/>
       </View>
     );
   }
